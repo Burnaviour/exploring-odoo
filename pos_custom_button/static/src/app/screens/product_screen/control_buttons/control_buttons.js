3e02ba432e6a91fe669664f060eb17807fac629f
Yoni <yoni.tjio@gmail.com>
2025-01-09 23:11:30 +0700

    Add POS Custom Button

---
+++ pos_custom_button/static/src/app/screens/product_screen/control_buttons/control_buttons.js
@@ -0,0 +1,7 @@
+import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+import { CustomButton } from "./custom_button/custom_button";
+
+ControlButtons.components = {
+    ...ControlButtons.components,
+    CustomButton
+}
